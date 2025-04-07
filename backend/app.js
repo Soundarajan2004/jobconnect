0f13d1335841f9d9b090aec2b810ab550d276ab3
Soundarajan <rsrsoundar91@gmail.com>
2025-04-07 22:40:42 +0530

    update app

--- backend/app.js
+++ backend/app.js
@@ -15,13 +15,7 @@ import fileUpload from "express-fileupload";
 const app = express();
 // config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
